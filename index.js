74ab7cfe817b2ff33016480659aa1c63975b1b44
bhanukirant99 <bhanukirant99@gmail.com>
2020-12-15 10:38:16 +0530

    feat: adding play() in index.js

--- index.js
+++ index.js
@@ -5,3 +5,18 @@ const userName = readlineSync.question('May I have your name? ');
 console.log('Hi ' + userName + ' Welcome to DO YOU KNOW JS?');
 console.log("If you wish to exit while playing press ctrl+c")
 
+function play(question, answer) {
+  var userAnswer = readlineSync.question(question, answer);
+  
+  if (userAnswer === answer) {
+    console.log("Correct answer!");
+    console.log(score+=1);
+  } else {
+      console.log("Oops! Wrong answer");
+      if (score > 0) {
+        console.log(score-=1);
+      }
+  }
+
+  console.log("--------------------------");
+}
