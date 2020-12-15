77486f7855174e7ddae8fba04d17ccac17df376a
bhanukirant99 <bhanukirant99@gmail.com>
2020-12-15 10:39:37 +0530

    feat: adding questions in index.js

--- index.js
+++ index.js
@@ -20,3 +20,26 @@ function play(question, answer) {
 
   console.log("--------------------------");
 }
+
+const questions = [{
+  question: "What do we use to print a value to the console? ",
+  answer: "console.log()"
+}, {
+  question: "Which built-in method combines the text of two strings and returns a new string? ",
+  answer: "concat()"
+}, {
+  question: "Which function of Number object returns the number's value? ",
+  answer: "valueOf()"
+}, {
+  question: "Which function of String object splits a String object into an array of strings by separating the string into substrings? ",
+  answer: "split()"
+}, {
+  question: "Which function of String object returns the calling string value converted to upper case? ",
+  answer: "toUpperCase()"
+}, {
+  question: "Which function of Array object removes the last element from an array and returns that element? ",
+  answer: "pop()"
+}, {
+  question: "Which function of Array object adds one or more elements to the front of an array and returns the new length of the array? ",
+  answer: "unshift()"
+}]
